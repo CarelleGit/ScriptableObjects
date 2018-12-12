9f74713234d8b371ed1924ab4cecbea850d3b2b1
Carelle <steamtik@gmail.com>
2018-12-12 13:08:13 -0800

    Initial commit

---
+++ Assets/Skills.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Skill", menuName = "Create a Skill",order = 1)]
+public class Skills : ScriptableObject
+{
+    public string skillName;
+    public Sprite skillIcon;
+    public int manacosts;
+    public Button skillButton; 
+    [Header("Requirements")]
+    public int level;
+    public string[] SkillRequired;
+    [Header("Skill State Check")]
+    public bool unlockable = false;
+    public bool unlcoked = false;
+}
